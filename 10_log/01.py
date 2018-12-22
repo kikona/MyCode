c2b65d40da4a0f21c5afd7ce614d1dde108bf238
kikona <kyq3.1415926>
2018-12-22 22:41:32 +0800

    更新

--- 10_log/01.py
+++ 10_log/01.py
@@ -1,5 +1,11 @@
 import logging
 
+# 自定义日志输出格式
+LOG_FORMAT = "%(asctime)s=====%(levelname)s++++++%(message)s"
+
+# 配置 logging
+logging.basicConfig(filename="rizhi.log", level=logging.DEBUG, format=LOG_FORMAT)
+
 logging.debug("This is a debug log.")
 logging.info("This is a info log.")
 logging.warning("This is a warning log.")
