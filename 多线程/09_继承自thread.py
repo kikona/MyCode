9421fc466ed49bc2a6602f448444245d12417f5c
kikona <kyq3.1415926>
2019-03-23 16:24:01 +0800

    更新

--- 多线程/09_继承自thread.py
+++ 多线程/09_继承自thread.py
@@ -10,7 +10,7 @@ class MyThread(threading.Thread):
     # 2.必须重写 run函数，run函数代表的是真正执行的功能
     def run(self):
         time.sleep(2)
-        print("The args for this class is {0}".format(self, arg))
+        print("The args for this class is {0}".format(self.arg))
 
 for i in range(5):
     t = MyThread(i)
